5649dcc77287aceb509288c74c7cb985cc91defd
Matthew Calder <matt@Matthews-MacBook-Pro.local>
2014-02-07 03:31:58 -0800

    Changed RPC dependency to xmlrpclib. Added support for credentials file.

--- plc_slice_renew.py
+++ plc_slice_renew.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/env python
 
 """Simple script to renew a slice using the PLCAPI python library.
 
@@ -8,9 +8,7 @@ Tested with Python 2.7 and PLCAPI version HEAD on Feb 28, 2012 on OS X.
 ##############################################################################
 ##############################################################################
 
-import sys, optparse, getpass, datetime, time
-
-import PLC.Shell
+import sys, optparse, getpass, datetime, time, xmlrpclib, os
 
 ##############################################################################
 ##############################################################################
@@ -21,8 +19,13 @@ RENEW_DAYS_DEFAULT = 54
 
 PLCAPI_URL = 'https://www.planet-lab.org/PLCAPI/'
 
+AUTH_FILE = '~/.pl_auth'
+
 USAGE = "%prog [options] USER"
 DESCRIPTION = """Renews a PlanetLab slice using the PLCAPI.
+First looks for credentials file at ~/.pl_auth formatted as
+"user password slice". If no file is found then USER is required.
+
 Required argument USER: PLC user or file containing PLC user.
 Prompts for the password if not given on the command line.
 Will renew some slice if no slice is specified.
@@ -32,26 +35,25 @@ Will renew some slice if no slice is specified.
 ##############################################################################
 
 def renew(url, user, password, slice, days):
-    shell = PLC.Shell.Shell(globals = globals(),
-                            url = url, 
-                            xmlrpc = True,
-                            method = 'password',
-                            user = user, 
-                            password = password)
+    
+    server = xmlrpclib.ServerProxy(PLCAPI_URL)
+
+    auth = {'Username': user, 'AuthString': password, 'AuthMethod': 'password'}
+
     if slice is not None:
-        slice = GetSlices(slice)[0]
+        slice = server.GetSlices(auth, slice)[0]
     else: # if slice is not specified, use the first slice returned
-        slice = GetSlices()[0]
+        slice = server.GetSlices(auth)[0]
     
     renew = datetime.timedelta(days=days)
     now = datetime.datetime.now()
     then = now + renew
     args = { 'expires' : int(time.mktime(then.timetuple())), }
     
-    if UpdateSlice(slice['name'], args) != 1:
+    if server.UpdateSlice(auth, slice['name'], args) != 1:
         raise RuntimeError('Unable to update slice %s' % slice)
 
-    slice = GetSlices(slice['name'])[0]
+    slice = server.GetSlices(auth, slice['name'])[0]
     return slice
 
 ##############################################################################
@@ -75,28 +77,43 @@ def main(argv=None):
     parser.add_option("-d", "--days",
                       type='int',
                       help="Days to renew (default: %d)" % RENEW_DAYS_DEFAULT,
-                      default=RENEW_DAYS_DEFAULT)
-    
+                      default=RENEW_DAYS_DEFAULT)    
+    parser.add_option("-f", "--file", default=AUTH_FILE, 
+                      help="Path to credentials file (default: %s)" % AUTH_FILE)
+
     opts, args = parser.parse_args()
-    if len(args) < 1:
-        parser.error("Missing required argument (USER)")
-    opts.user = args[0]
-    try:
-        opts.user = open(opts.user).read().strip()
-    except IOError:
-        pass
-    if opts.password is None:
+    if len(args) == 0:          #should have AUTH_FILE configured
         try:
-            opts.password = getpass.getpass()
-        except (EOFError, KeyboardInterrupt):
-            return 0
-    else:
+            auth_file_path = os.path.expanduser(opts.file)
+            credentials = open(auth_file_path).read().strip().split()
+            opts.user = credentials[0]
+            opts.password = credentials[1]
+            opts.slice = credentials[2]
+        except IOError:
+            parser.error('Missing required argument (USER) or no %s file configured' % AUTH_FILE)
+        except IndexError:
+            parser.error('Format of %s file is "user password slice"' % AUTH_FILE)
+    elif len(args) == 1:
+        opts.user = args[0]
         try:
-            opts.password = open(opts.password).read().strip()
+            opts.user = open(opts.user).read().strip()
         except IOError:
             pass
+        if opts.password is None:
+            try:
+                opts.password = getpass.getpass()
+            except (EOFError, KeyboardInterrupt):
+                return 0
+        else:
+            try:
+                opts.password = open(opts.password).read().strip()
+            except IOError:
+                pass
+    else:
+        parser.error('Missing required argument (USER) or no %s file configured' % AUTH_FILE)
 
-    slice = renew(**(opts.__dict__))
+    #slice = renew(**(opts.__dict__))
+    slice = renew(opts.url, opts.user, opts.password, opts.slice, opts.days)
     expires = time.strftime("%d %B %Y %H:%M:%S", 
                             time.localtime(slice['expires']))
     # Print result
